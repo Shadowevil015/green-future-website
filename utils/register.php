8c53602c8663365eca5ff99c6d5c72dc0b2ec2a1
Shadowevil015 <64799983+Shadowevil015@users.noreply.github.com>
2024-10-22 11:15:15 +0100

    Fix incorrect SQL statement

--- utils/register.php
+++ utils/register.php
@@ -12,7 +12,7 @@ $password = password_hash($password, PASSWORD_DEFAULT);
 $conn = OpenCon();
 
 $statement = $conn->prepare('INSERT INTO accounts (first_name, last_name, email_address, password) VALUES (?, ?, ?, ?)');
-$statement->bind_param('ssssi', $firstName, $lastName, $email, $password);
+$statement->bind_param('ssss', $firstName, $lastName, $email, $password);
 
 $statement->execute();
 
